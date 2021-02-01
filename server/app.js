5da2edf49f59c7ad5a0f2792008da23ae2617b97
Amiruriza <getvpstandvp@gmail.com>
2021-02-01 13:10:37 +0700

    minus validation

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+
+app.listen(port, () => {
+    console.log('this is listening to ', port);
+})
---
+++ server/models/todo.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Todo.init({
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    status: DataTypes.BOOLEAN,
+    due_date: {
+      type: DataTypes.DATE,
+      // validate: {
+      //   isAfter: {
+      //     args: new Date().getTime(),
+      //     msg: 'Tidak boleh memasukkan tanggal yg sudah lewat'
+      //   }
+      // }
+    }
+  }, {
+    sequelize,
+    modelName: 'Todo',
+  });
+  return Todo;
+};
