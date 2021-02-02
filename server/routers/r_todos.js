fc0c0f40e2d78635eada09969916a7a51b6ca364
kudaer69 <clvnyd96@gmail.com>
2021-02-02 10:15:57 +0700

    add update, delete, and patch router

--- server/routers/r_todos.js
+++ server/routers/r_todos.js
@@ -3,9 +3,15 @@ const C_Todos = require('../controllers/c_todos');
 
 router.get('/', C_Todos.read);
 
-router.post('/add', C_Todos.create);
+router.post('/', C_Todos.create);
 
 router.get('/:id', C_Todos.filter_id);
+
+router.put('/:id', C_Todos.update);
+
+router.patch('/id', C_Todos.update_status)
+
+router.delete('id', C_Todos.delete);
 
 
 module.exports = router;
