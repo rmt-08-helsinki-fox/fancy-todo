14ed6dfe6dde4cf26e744580ce0d64385e83df1c
lthfiazhari <>
2021-02-02 19:29:38 +0700

    authorize works

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -1,12 +1,32 @@
-const authorize = function(req, res, next) {
-  if (req.decode) {
-    console.log(req.decode);
-    next()
-  } else {
-    res.status(401).json({
-      message: 'Not authorize'
-    })
-  }
+const {Todo, User} = require('../models')
+
+const authorizeTodo = function(req, res, next) {
+  let id = req.params.id;
+  let decodeId = req.decode.id
+  // console.log('masuk authorize');
+  // console.log(id, 'id');
+  Todo.findOne({
+    where: {
+      id: id
+    }
+  }).then(function(data) {
+    // console.log(data,'data');
+    if (!data) {
+      next({
+        stat: 404,
+        message: 'Data not found'
+      })
+    } else if (data.user_id === decodeId) {
+      next()
+    } else if (data.user_id !== decodeId) {
+      next({
+        stat: 401,
+        message: 'Not authorize'
+      })
+    }
+  }).catch(function(err) {
+    res.status(500).json(err)
+  })
 }
 
-module.exports = authorize
+module.exports = authorizeTodo
--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -2,9 +2,9 @@
 require('dotenv').config()
 const router = require('express').Router()
 const TodoController = require('../controllers/todoController')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 const authentication = require('../middleware/authentication')
-const authorize = require('../middleware/authorize')
+const authorizeTodo = require('../middleware/authorize')
 
 
 router.use(authentication)
@@ -12,11 +12,10 @@ router.use(authentication)
 router.post('/', TodoController.addTodo)
 router.get('/', TodoController.getAll)
 
-router.use(authorize)
-router.get('/:id', TodoController.getById)
-router.put('/:id', TodoController.editPutById)
-router.patch('/:id', TodoController.editPatchById)
-router.delete('/:id', TodoController.destroy)
+router.get('/:id', authorizeTodo, TodoController.getById)
+router.put('/:id', authorizeTodo, TodoController.editPutById)
+router.patch('/:id', authorizeTodo, TodoController.editPatchById)
+router.delete('/:id', authorizeTodo, TodoController.destroy)
 
 
 module.exports = router
