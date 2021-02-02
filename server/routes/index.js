7bfcc54640946f8a9ce007682a4965a08c976db1
jefDimar <jefdimar@gmail.com>
2021-02-02 14:07:45 +0700

    modify routes adding auth and authorize

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,21 @@
 const routes = require('express').Router()
 const UserController = require('../controllers/userController')
 const TodoController = require('../controllers/todoController')
+const authentication = require('../middlewares/authentication')
+const authorize = require('../middlewares/authorization')
 
 // User routing
 routes.post('/login', UserController.login)
 routes.post('/register', UserController.register)
 
+routes.use(authentication)
+
 // Todo routing
 routes.get('/todos', TodoController.showAll)
 routes.post('/todos', TodoController.create)
-routes.put('/todos/:id', TodoController.update)
-routes.patch('/todos/:id', TodoController.updateStatus)
-routes.delete('/todos/:id', TodoController.delete)
+routes.get('/todos/:id', authorize, TodoController.showByID)
+routes.put('/todos/:id', authorize, TodoController.update)
+routes.patch('/todos/:id', authorize, TodoController.updateStatus)
+routes.delete('/todos/:id', authorize, TodoController.delete)
 
 module.exports = routes
