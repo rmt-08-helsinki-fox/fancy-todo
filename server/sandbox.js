1c09a7228185ece0b31cf0dcc81b30bf0a910895
ezekielhutagalung <yehezkiel.life@gmail.com>
2021-02-03 00:03:58 +0700

    app3rdappselesai

--- server/sandbox.js
+++
@@ -1,4 +0,0 @@
-// var currentDate = new Date();
-// console.log("The current date="+currentDate);
-var yesterdayDate = new Date().setDate(new Date().getDate()- 1);
-console.log("The yesterday date ="+new Date(yesterdayDate));
