d86375a60944f500991f35ad32981100bcfbbb3a
Luthfi Azhari <m.luthfiazhari@gmail.com>
2021-02-02 11:33:42 +0700

    delete tike test.js. tadi lupa di hapus

--- server/test.js
+++
@@ -1,9 +0,0 @@
-const jwtHelper = require('./helpers/jsonwebtoken.js');
-function test() {
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImR1c3VuLm5lbGF5YW5AZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmEkMTAkZ0FLbDhJc1o2VkRVMUFvWHMuUEllZWh1bVFmd3g4dVYuRkRwYUhxT2ZFWWZ3RFFNcExhSnUiLCJpYXQiOjE2MTIxOTUwNTB9.tE-GdGnyUg_6WY3Zk9d74WAQV9-Hv-JVJyUGjapuMZI';
-
-  const result = jwtHelper.verifyToken(token);
-  console.log(result.email)
-}
-
-test();
