7fca4255e467e4e477184b9e7da963414ddc3a8a
iqballbaihaqi <baihaqiiqbal323@gmail.com>
2021-02-03 15:13:12 +0700

    add cors

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const express = require('express');
+const express = require("express");
 const app = express();
-const route = require('./routes')
-const errorHandler = require('./helpers/errorHandler')
+const route = require("./routes");
+const errorHandler = require("./helpers/errorHandler");
+const cors = require('cors')
 const PORT = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(route)
-app.use(errorHandler)
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(route);
+app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`server running at port ${PORT}`))
+app.listen(PORT, () => console.log(`server running at port ${PORT}`));
