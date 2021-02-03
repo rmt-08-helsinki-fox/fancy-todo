b20e25975f7deb23528f643e1e37ab4405f626b0
kudaer69 <clvnyd96@gmail.com>
2021-02-03 15:54:18 +0700

    fix some errors in main router

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const todos = require('./r_todos');
+const todos = require('./todosRouter');
+const user = require('./userRouter');
 const Controller = require('../controllers');
 
 router.get('/', Controller.main);
 router.use('/todos', todos);
+router.use('/user', user);
 
 module.exports = router;
