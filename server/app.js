515939126f9aba64e13773009f3f496a3c1b6ced
mrdanang <mrdanang4108@gmail.com>
2021-02-03 18:27:50 +0700

    progres day 3

--- server/app.js
+++ server/app.js
@@ -5,9 +5,11 @@ if(process.env.NODE_ENV === 'development'){
 const express = require('express');
 const app = express();
 const router = require('./routes/index.js');
+const cors = require('cors');
 
 const port = 3000;
 
+app.use(cors())
 app.use(express.urlencoded({ extended : true }))
 
 app.use(router)
