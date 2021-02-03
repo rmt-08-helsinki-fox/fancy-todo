87678d11c4b3fb019e79d9788b131bff75f36670
lthfiazhari <>
2021-02-03 13:05:57 +0700

    err handler

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -5,29 +5,28 @@ const authorizeTodo = function(req, res, next) {
   let decodeId = req.decode.id
   // console.log('masuk authorize');
   // console.log(id, 'id');
-  next()
-  // Todo.findOne({
-  //   where: {
-  //     id: id
-  //   }
-  // }).then(function(data) {
-  //   // console.log(data,'data');
-  //   if (!data) {
-  //     next({
-  //       stat: 404,
-  //       message: 'Data not found'
-  //     })
-  //   } else if (data.user_id === decodeId) {
-  //     next()
-  //   } else if (data.user_id !== decodeId) {
-  //     next({
-  //       stat: 401,
-  //       message: 'Not authorize'
-  //     })
-  //   }
-  // }).catch(function(err) {
-  //   res.status(500).json(err)
-  // })
+  Todo.findOne({
+    where: {
+      id: id
+    }
+  }).then(function(data) {
+    // console.log(data,'data');
+    if (!data) {
+      next({
+        stat: 404,
+        message: 'Data not found'
+      })
+    } else if (data.user_id === decodeId) {
+      next()
+    } else if (data.user_id !== decodeId) {
+      next({
+        stat: 401,
+        message: 'Not authorize'
+      })
+    }
+  }).catch(function(err) {
+    res.status(500).json(err)
+  })
 }
 
 module.exports = authorizeTodo
--- server/middleware/errHandler.js
+++ server/middleware/errHandler.js
@@ -1,5 +1,9 @@
 function errHandler(err, req, res, next) {
-  if (err.errors[0].type === 'Validation error') {
+  if (err.message === 'Not authorize') {
+    res.status(401).json(err.message)
+  } else if (err.message === 'Data not found') {
+    res.status(404).json(err.message)
+  } else if (err.errors[0].type === 'Validation error') {
     res.status(400).json(err)
   } else {
     res.status(500).json(err)
