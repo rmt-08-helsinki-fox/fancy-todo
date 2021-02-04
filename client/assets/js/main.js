fef7a8a1f9e027c42f24f789e440498902f88214
Ramadhan Nur Cholis Januar <adhann98@gmail.com>
2021-02-04 09:00:53 +0700

    fixed main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -114,61 +114,61 @@
         todos.forEach(el => {
           $("#todoList").append(`
           <div class="input-group mb-3" id="todo-${el.id}">
-          <div class="card col">
-          <div class="card-body" id="todoCardBody_182">
-          <div id="todoCardValue_182">
-          <button type="button" data-toggle="modal" data-target="#modal-todo-${el.id}" class="btn fas fa-pencil-alt text-info border border-info float-right m-1"></button>
-          <a href="#" onclick="swalDelete(${el.id})" class="btn fas fa-trash-alt text-danger border border-danger float-right m-1"></a>
-          <h3 class="card-title">${el.title}</h3>
-          <h6 class="card-subtitle mb-2 text-muted">${el.due_date}</h6>
-          <p class="card-text">${el.description}</p>
-          <p class="card-text">${el.status}</p>
-          </div>
-          </div>
-          </div>
+            <div class="card col">
+              <div class="card-body">
+                <div>
+                  <button type="button" data-toggle="modal" data-target="#modal-todo-${el.id}" class="btn fas fa-pencil-alt text-info border border-info float-right m-1"></button>
+                  <a href="#" onclick="swalDelete(${el.id})" class="btn fas fa-trash-alt text-danger border border-danger float-right m-1"></a>
+                  <h3 class="card-title">${el.title}</h3>
+                  <h6 class="card-subtitle mb-2 text-muted">${el.due_date}</h6>
+                  <p class="card-text">${el.description}</p>
+                  <p class="card-text">${el.status}</p>
+                </div>
+              </div>
+            </div>
           </div>
           
           <!-- Modal -->
           <div class="modal" id="modal-todo-${el.id}" tabindex="-1" aria-hidden="true">
-          <div class="modal-dialog">
-          <div class="modal-content">
-          <div class="modal-header">
-          <h5 class="modal-title">Modal ${el.title}</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-          </button>
-          </div>
-          <form action="">
-          <div class="modal-body">
-          <div class="form-group">
-          <label for="inputModalTitle-${el.id}">Title</label>
-          <input type="text" class="form-control" id="inputModalTitle-${el.id}" value="${el.title}" required>
-          </div>
-          <div class="form-group">
-          <label for="inputModalDescription-${el.id}">Description</label>
-          <input type="text" class="form-control" id="inputModalDescription-${el.id}" value="${el.description}" required>
-          </div>
-          <div class="form-group">
-          <label for="inputModalStatus-${el.id}">Status</label>
-          <select class="form-control" id="inputModalStatus-${el.id}" required>
-          <option value="" selected>Choose</option>
-          <option value="false">False</option>
-          <option value="true">True</option>
-          </select>
-          </div>
-          <div class="form-group">
-          <label for="inputModalDuedate-${el.id}">Due Date</label>
-          <input type="date" class="form-control" id="inputModalDuedate-${el.id}" value="${el.due_date}" required>
-          
-          </div>
-          </div>
-          <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary" onclick="update(${el.id})">Save changes</button>
-          </div>
-          </form>
-          </div>
-          </div>
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modal ${el.title}</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <form action="">
+                  <div class="modal-body">
+                    <div class="form-group">
+                      <label for="inputModalTitle-${el.id}">Title</label>
+                      <input type="text" class="form-control" id="inputModalTitle-${el.id}" value="${el.title}" required>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputModalDescription-${el.id}">Description</label>
+                      <input type="text" class="form-control" id="inputModalDescription-${el.id}" value="${el.description}" required>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputModalStatus-${el.id}">Status</label>
+                      <select class="form-control" id="inputModalStatus-${el.id}" required>
+                        <option value="" selected>Choose</option>
+                        <option value="false">False</option>
+                        <option value="true">True</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputModalDuedate-${el.id}">Due Date</label>
+                      <input type="date" class="form-control" id="inputModalDuedate-${el.id}" value="${el.due_date}" required>
+                      
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="update(${el.id})">Save changes</button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
           `)
         })
@@ -203,60 +203,60 @@
       .done((todo) => {
         $("#todoList").append(`
         <div class="input-group mb-3" id="todo-${todo.id}">
-        <div class="card col">
-        <div class="card-body" id="todoCardBody_182">
-        <div id="todoCardValue_182">
-        <button type="button" data-toggle="modal" data-target="#modal-todo-${todo.id}" class="btn fas fa-pencil-alt text-info border border-info float-right m-1"></button>
-        <a href="#" onclick="swalDelete(${todo.id})" class="btn fas fa-trash-alt text-danger border border-danger float-right m-1"></a>
-        <h3 class="card-title">${todo.title}</h3>
-        <h6 class="card-subtitle mb-2 text-muted">${todo.due_date}</h6>
-        <p class="card-text">${todo.description}</p>
-        <p class="card-text">${todo.status}</p>
-        </div>
-        </div>
-        </div>
+          <div class="card col">
+            <div class="card-body">
+              <div>
+                <button type="button" data-toggle="modal" data-target="#modal-todo-${todo.id}" class="btn fas fa-pencil-alt text-info border border-info float-right m-1"></button>
+                <a href="#" onclick="swalDelete(${todo.id})" class="btn fas fa-trash-alt text-danger border border-danger float-right m-1"></a>
+                <h3 class="card-title">${todo.title}</h3>
+                <h6 class="card-subtitle mb-2 text-muted">${todo.due_date}</h6>
+                <p class="card-text">${todo.description}</p>
+                <p class="card-text">${todo.status}</p>
+              </div>
+            </div>
+          </div>
         </div>
         
         <!-- Modal -->
         <div class="modal" id="modal-todo-${todo.id}" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-        <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Modal ${el.title}</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-        </button>
-        </div>
-        <form action="">
-        <div class="modal-body">
-        <div class="form-group">
-        <label for="inputModalTitle-${todo.id}">Title</label>
-        <input type="text" class="form-control" id="inputModalTitle-${todo.id}" value="${todo.title}" required>
-        </div>
-        <div class="form-group">
-        <label for="inputModalDescription-${todo.id}">Description</label>
-        <input type="text" class="form-control" id="inputModalDescription-${todo.id}" value="${todo.description}" required>
-        </div>
-        <div class="form-group">
-        <label for="inputModalStatus-${todo.id}">Status</label>
-        <select class="form-control" id="inputModalStatus-${todo.id}" required>
-        <option value="" selected>Choose</option>
-        <option value="false">False</option>
-        <option value="true">True</option>
-        </select>
-        </div>
-        <div class="form-group">
-        <label for="inputModalDuedate-${todo.id}">Due Date</label>
-        <input type="date" class="form-control" id="inputModalDuedate-${todo.id}" value="${todo.due_date}" required>
-        </div>
-        </div>
-        <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" onclick="update(${todo.id})">Save changes</button>
-        </div>
-        </form>
-        </div>
-        </div>
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Modal ${el.title}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form action="">
+                <div class="modal-body">
+                  <div class="form-group">
+                    <label for="inputModalTitle-${todo.id}">Title</label>
+                    <input type="text" class="form-control" id="inputModalTitle-${todo.id}" value="${todo.title}" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputModalDescription-${todo.id}">Description</label>
+                    <input type="text" class="form-control" id="inputModalDescription-${todo.id}" value="${todo.description}" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputModalStatus-${todo.id}">Status</label>
+                    <select class="form-control" id="inputModalStatus-${todo.id}" required>
+                      <option value="" selected>Choose</option>
+                      <option value="false">False</option>
+                      <option value="true">True</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputModalDuedate-${todo.id}">Due Date</label>
+                    <input type="date" class="form-control" id="inputModalDuedate-${todo.id}" value="${todo.due_date}" required>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary" onclick="update(${todo.id})">Save changes</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
         `)          
       })
