4367586145f4bbd42a9da1ba825b5439a381e06f
Rofi Akmaluddin <r.akmaluddin@gmail.com>
2021-02-04 08:09:46 +0700

    jquery & ajax uncompleted 2

--- client/helpers/helper.js
+++ client/helpers/helper.js
@@ -1,4 +1,3 @@
-const baseUrl = 'http://localhost:3000/'
 const auth = () => {
   if (!localStorage.getItem("access_token")) {
     $("#sign-out-navbar").hide()
@@ -17,6 +16,7 @@ const auth = () => {
     $("#sign-in-page").hide()
     $("#addTodo-page").hide()
     $("#todos-table").show()
+    getAllTodo()
   }
 }
 
@@ -32,6 +32,210 @@ const signIn = () => {
       password
     }
   })
+  .done(res => {
+    localStorage.setItem('access_token',res.access_token)
+    auth()
+  })
+  .fail((xhr,text) => {
+    console.log(xhr, text);
+  })
+  .always(_ => {
+    $('#signInEmail').val('')
+    $('#signInPass').val('')
+  })
 }
 
-module.exports = auth
+const signUp = () => {
+  const email = $('#signUpEmail').val()
+  const password = $('#signUpPass').val()
+
+  $.ajax({
+    url: baseUrl + 'signUp',
+    method: 'POST',
+    data: {
+      email,
+      password
+    }
+  })
+  .done(res => {
+    console.log(res.msg);
+    auth()
+  })
+  .fail((xhr, text) => {
+    console.log(xhr, text);
+  })
+  .always(_ => {
+    $('#signUpEmail').val('')
+    $('#signUpPass').val('')
+  })
+}
+
+const signOut = () => {
+  localStorage.clear()
+  auth()
+}
+
+const getAllTodo = () => {
+  $.ajax({
+    url: baseUrl + 'todos',
+    method: 'GET',
+    headers: {
+      access_token: localStorage.getItem('access_token')
+    }
+  })
+  .done(res => {
+    $('#tBody').empty()
+    res.forEach(e => {
+      let btnDone
+      if (!e.status) {
+        btnDone = `<button type="button" class="btn btn-success" onClick="${updateStatusTodo(e.id,e.status)}">Done</button>`
+      } else {
+        btnDone = `<button type="button" class="btn btn-success" onClick="${updateStatusTodo(e.id,e.status)}">Undone</button>`
+      }
+      $('#tBody').append(`<tr>
+      <td>${e.title}</td>
+      <td>${e.description}</td>
+      <td>${e.status}</td>
+      <td>${e.due_date}</td>
+      <td>${e.User}</td>
+      <td>
+        <button type="button" class="btn btn-warning" onClick="${updateTodo(e.id)}">Edit</button> 
+        ${btnDone}
+        <button type="button" class="btn btn-danger" onClick="${destroy(e.id)}">Delete</button>
+      </td>
+    </tr>`)
+    });
+  })
+  .fail((xhr,txt) => {
+    console.log(xhr,txt);
+  })
+}
+
+const getTodoById = (id) => {
+  $.ajax({
+    url: baseUrl + `todos/${id}`,
+    method: 'GET',
+    headers: {
+      access_token: localStorage.getItem('access_token')
+    }
+  })
+  .done(res => {
+    $("#sign-out-navbar").show()
+    $("#sign-in-navbar").hide()
+    $("#sign-up-navbar").hide()
+    $("#sign-up-page").hide()
+    $("#sign-in-page").hide()
+    $("#addTodo-page").hide()
+    $("#todos-table").show()
+    $('#tBody').empty()
+    let btnDone
+      if (!res.status) {
+        btnDone = `<button type="button" class="btn btn-success" onClick="${updateStatusTodo(res.id,res.status)}">Done</button>`
+      } else {
+        btnDone = `<button type="button" class="btn btn-success" onClick="${updateStatusTodo(res.id,res.status)}">Undone</button>`
+      }
+      $('#tBody').append(`<tr>
+      <td>${res.title}</td>
+      <td>${res.description}</td>
+      <td>${res.status}</td>
+      <td>${res.due_date}</td>
+      <td>${res.User}</td>
+      <td>
+        <button type="button" class="btn btn-warning" onClick="${updateTodo(res.id)}">Edit</button> 
+        ${btnDone}
+        <button type="button" class="btn btn-danger" onClick="${destroy(res.id)}">Delete</button>
+      </td>
+    </tr>`)
+  })
+}
+
+const createTodo = () => {
+  const title = $('#titleAdd').val()
+  const description = $('#descriptionAdd').val()
+  const due_date = $('#due_dateAdd').val()
+  const status = false
+
+  $.ajax({
+    url: baseUrl + 'todos',
+    method: 'POST',
+    data:{
+      title,
+      description,
+      due_date,
+      status
+    },
+    headers: {
+      access_token: localStorage.getItem('access_token')
+    }
+  })
+  .done(res => {
+    auth()
+  })
+  .fail((xhr,txt) => {
+    console.log(xhr,txt);
+  })
+}
+
+const updateTodo = (id) => {
+  const title = $('#titleAdd').val()
+  const description = $('#titleAdd').val()
+  const due_date = $('#titleAdd').val()
+  const status = false
+
+  $.ajax({
+    url: baseUrl + `todos/${id}`,
+    method: 'PUT',
+    data: {
+      title,
+      description,
+      due_date,
+      status
+    },
+    headers: {
+      access_token: localStorage.getItem('access_token')
+    }
+  })
+  .done(res => {
+    auth()
+  })
+  .fail((xhr,txt) => {
+    console.log(xhr,txt);
+  })
+}
+
+const updateStatusTodo = (id,status) => {
+  status ? status = false : status = true
+
+  $.ajax({
+    url: baseUrl + `todos/${id}`,
+    method: 'PATCH',
+    data: {
+      status
+    },
+    headers: {
+      access_token: localStorage.getItem('access_token')
+    }
+  })
+  .done(res => {
+    auth()
+  })
+  .fail((xhr,txt) => {
+    console.log(xhr,txt);
+  })
+}
+
+const destroy = id => {
+  $.ajax({
+    url: baseUrl + `todos/${id}`,
+    method: 'DELETE',
+    headers: {
+      access_token: localStorage.getItem('access_token')
+    }
+  })
+  .done(_ => {
+    auth()
+  })
+  .fail((xhr, txt) => {
+    console.log(xhr,txt);
+  })
+}
--- client/script/script.js
+++ client/script/script.js
@@ -1,122 +1,10 @@
-// const auth = require("../helpers/helper.js");
 const baseUrl = 'http://localhost:3000/'
-const auth = () => {
-  if (!localStorage.getItem("access_token")) {
-    $("#sign-out-navbar").hide()
-    $("#sign-in-navbar").show()
-    $("#sign-up-navbar").show()
-    $("#sign-up-page").hide()
-    $("#sign-in-page").show()
-    $("#addTodo-page").hide()
-    $("#todos-table").hide()
-
-  } else {
-    $("#sign-out-navbar").show()
-    $("#sign-in-navbar").hide()
-    $("#sign-up-navbar").hide()
-    $("#sign-up-page").hide()
-    $("#sign-in-page").hide()
-    $("#addTodo-page").hide()
-    $("#todos-table").show()
-    getAllTodo()
-  }
-}
-
-const signIn = () => {
-  const email = $('#signInEmail').val()
-  const password = $('#signInPass').val()
-
-  $.ajax({
-    url: baseUrl + 'signIn',
-    method: 'POST',
-    data: {
-      email,
-      password
-    }
-  })
-  .done(res => {
-    localStorage.setItem('access_token',res.access_token)
-    auth()
-  })
-  .fail((xhr,text) => {
-    console.log(xhr, text);
-  })
-  .always(_ => {
-    $('#signInEmail').val('')
-    $('#signInPass').val('')
-  })
-}
-
-const signUp = () => {
-  const email = $('#signUpEmail').val()
-  const password = $('#signUpPass').val()
-
-  $.ajax({
-    url: baseUrl + 'signUp',
-    method: 'POST',
-    data: {
-      email,
-      password
-    }
-  })
-  .done(res => {
-    console.log(res.msg);
-  })
-  .fail((xhr, text) => {
-    console.log(xhr, text);
-  })
-  .always(_ => {
-    $('#signUpEmail').val('')
-    $('#signUpPass').val('')
-  })
-}
-
-const getAllTodo = () => {
-  $.ajax({
-    url: baseUrl + 'todos',
-    method: 'GET',
-    headers: {
-      access_token: localStorage.getItem('access_token')
-    }
-  })
-  .done(res => {
-    $('#tBody').empty()
-    res.forEach(e => {
-      $('#tBody').append(`<tr>
-      <td>${e.title}</td>
-      <td>${e.description}</td>
-      <td>${e.status}</td>
-      <td>${e.due_date}</td>
-      <td>${e.User}</td>
-      <td><button type="button" class="btn btn-success" onClick="${destroy(e.id)}">Done</button></td>
-    </tr>`)
-    });
-  })
-  .fail((xhr,text) => {
-
-  })
-}
-
-const destroy = id => {
-  $.ajax({
-    url: baseUrl + `todos/${id}`,
-    method: 'DELETE',
-    headers: {
-      access_token: localStorage.getItem('access_token')
-    }
-  })
-  .done(_ => {
-    getAllTodo()
-  })
-  .fail((xhr, text) => {
-
-  })
-}
-
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 $(document).ready(function(){
+
   auth()
+  
   $('#navbarSignIn').click(() => {
     $("#sign-out-navbar").hide()
     $("#sign-in-navbar").show()
@@ -126,6 +14,7 @@ $(document).ready(function(){
     $("#addTodo-page").hide()
     $("#todos-table").hide()
   })
+
   $('#navbarSignUp').click(() => {
     $("#sign-out-navbar").hide()
     $("#sign-in-navbar").show()
@@ -135,7 +24,10 @@ $(document).ready(function(){
     $("#addTodo-page").hide()
     $("#todos-table").hide()
   })
+
   $('#navbarSignOut').click(() => {
+    signOut()
+
     $("#sign-out-navbar").hide()
     $("#sign-in-navbar").show()
     $("#sign-up-navbar").show()
@@ -144,14 +36,17 @@ $(document).ready(function(){
     $("#addTodo-page").hide()
     $("#todos-table").hide()
   })
+
   $('#signInForm').submit((e) => {
     e.preventDefault()
-
+    signIn()
   })
+
   $('#signUpForm').submit((e) => {
     e.preventDefault()
-    
+    signUp()
   })
+
   $('#addTodoBtn').click(() => {
     $("#sign-out-navbar").show()
     $("#sign-in-navbar").hide()
@@ -161,8 +56,9 @@ $(document).ready(function(){
     $("#addTodo-page").show()
     $("#todos-table").hide()
   })
+
   $('#addTodoForm').submit((e) => {
     e.preventDefault()
-    
+    createTodo()
   })
 });
