4959acc960cc3c0801b31bdf6ca802163142374b
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-02-04 07:34:32 +0700

    add google authenticator (not working yet)

--- client/script.js
+++ client/script.js
@@ -1,5 +1,13 @@
 const base_url = 'http://localhost:3000/';
 
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 function register() {
   const email = $("#registerEmail").val();
   const password = $("#registerPassword").val();
