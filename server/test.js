450ddc7fd465036bb416822104a5992d070a45cc
gifaniasofia <gifaniasofia@gmail.com>
2021-02-06 00:44:37 +0700

    delete file test

--- server/test.js
+++
@@ -1,9 +0,0 @@
-const date = "2021-02-07"
-const dateToday = "2021-02-06"
-if (dateToday < date) {
-  console.log(`bisa`);
-} else if (dateToday == date) {
-  console.log(`sama`);
-} else {
-  console.log(`gabisa`);
-}
