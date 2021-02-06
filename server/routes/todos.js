fb3767b9342dc3c4bcc94dcc8fc934a3eb57b71c
Steve Timbuleng <steve.timbuleng@gmail.com>
2021-02-06 17:38:53 +0700

    routes revision

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -2,11 +2,11 @@ const router = require('express').Router()
 const TodoController = require('../controllers/TodoController')
 const { authorization } = require('../middlewares/auth')
 
-router.post('/todos', TodoController.addTodo)
-router.get('/todos', TodoController.showTodos)
-router.get('/todos/:id', authorization, TodoController.showTodoId)
-router.put('/todos/:id', authorization, TodoController.editTodo)
-router.patch('/todos/:id', authorization, TodoController.editStatusTodo)
-router.delete('/todos/:id', authorization, TodoController.deleteTodo)
+router.post('/', TodoController.addTodo)
+router.get('/', TodoController.showTodos)
+router.get('/:id', authorization, TodoController.showTodoId)
+router.put('/:id', authorization, TodoController.editTodo)
+router.patch('/:id', authorization, TodoController.editStatusTodo)
+router.delete('/:id', authorization, TodoController.deleteTodo)
 
 module.exports = router
