735d86db1ffe2316b9172c2790079860a49a648f
ferdianatmadiputra <ferdian.atmadiputra@gmail.com>
2021-02-06 18:21:34 +0700

    updatesedikit

--- client/script.js
+++ client/script.js
@@ -205,7 +205,7 @@ function getTodos() {
               </div>
 
               <div class="col-auto">
-                <a onclick="getEditTodo(${id}, ${title}, ${description}, ${due_date}, ${status})" id="edit-task-${value.id}" class="btn btn-outline-primary btn-sm"> 
+                <a onclick="getEditTodo(${id},'${title}','${description}','${due_date}','${status}')" id="edit-task-${value.id}" class="btn btn-outline-primary btn-sm"> 
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -238,9 +238,9 @@ function getTodos() {
     })
 }
 
-function getEdit(id, title, description, due_date, status) {
-  $(`"#todo${id}"`).empty();
-  $(`"#todo${id}"`).append(`
+function getEditTodo(id, title, description, due_date, status) {
+  $(`#todo${id}`).empty();
+  $(`#todo${id}`).append(`
   <form  class="card-body p-3 form-inline row g-3 justify-content-center" id="editTodo">
   <div class="col-md-2">
     <input type="text" value="${title}" class="form-control mb-2" id="editTodoTitle" placeholder="Edit Todo Title">
@@ -268,8 +268,16 @@ function getEdit(id, title, description, due_date, status) {
   </div>
 </form>
   `)
-
+  .fail((xhr, text) => {
+      
+    console.log(xhr, text)
+  })
+  .always((_) => {
+    console.log('always getEdit')
+  })
 }
+
+
 
 
 //nanti dulu cari tau cara 
