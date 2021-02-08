9a641e620c457be010da71462eb7c708c8d32fb1
fathan-nurudin <fathan011197@gmail.com>
2021-02-08 21:45:24 +0700

    comment router /weather

--- server/middleware/errorhandler.js
+++ server/middleware/errorhandler.js
@@ -1,4 +1,4 @@
-module.exports = ((err, req, res, next) => {
-    console.log(err)
-    // if()
-})
+// module.exports = ((err, req, res, next) => {
+//     console.log(err)
+//     // if()
+// })
--- server/routes/router.js
+++ server/routes/router.js
@@ -9,7 +9,7 @@ const authenticate = require('../middleware/authenticate')
 router.use('/', users)
 router.use(authenticate)
 router.use('/todos', todos)
-router.get('/weather', Todo.getWeather)
+// router.get('/weather', Todo.getWeather)
 
 
 module.exports = router
