5ea1809cb06320e74a229ceb5de11de8d3006ec8
RJO-001 <josephromario@rocketmail.com>
2021-02-08 15:22:49 +0700

    minor improvement

--- client/script.js
+++ client/script.js
@@ -329,6 +329,7 @@ function remove(id) {
 }
 
 function logout(){
+    $("#todo-list").empty();
     localStorage.removeItem("token");
     let auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
