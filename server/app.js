2ddab57f6017d9106a7fdfa50208b06a306e8569
muhammadsuryo31 <muhammadsuryo31@gmail.com>
2021-02-09 06:35:43 +0700

    preparing deployment

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-const express = require("express")
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'development') {
+    console.log("hallo");
     require('dotenv').config()
 }
+const express = require("express")
 const cors = require("cors")
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require("./router/main")
 const errorHandler = require("./middlewares/errorHandler")
 
