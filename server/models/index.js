7e243ee716ed18ab12e2d06ff25be9a284841894
ardyanrizki <surel.ardy@gmail.com>
2021-02-08 17:47:04 +0700

    config 7

--- server/models/index.js
+++ server/models/index.js
@@ -11,12 +11,7 @@ const db = {}
 let sequelize
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
-    dialect: "postgres",
-    dialectOptions: {
-      "ssl": {
-        "rejectUnauthorized": false
-      }
-    }
+    dialect: "postgres"
   })
 } else {
   sequelize = new Sequelize(
