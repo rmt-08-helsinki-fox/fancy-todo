eda476c58b66bcca9b159542dbf81ee050934c52
hackerrank <me@hackerrank.com>
2021-02-08 19:14:50 +0700

    deploy heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 
-require('dotenv').config()
+if(process.env.NODE_ENV === 'development'){
+    require('dotenv').config()
+}
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
