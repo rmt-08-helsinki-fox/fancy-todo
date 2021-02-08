7caf0e5fa38e55362e70ff5b61e287e272ef7d89
fathan-nurudin <fathan011197@gmail.com>
2021-02-08 21:49:38 +0700

    comment some of variables

--- server/app.js
+++ server/app.js
@@ -7,9 +7,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const router = require('./routes/router')
-const errorHandler = require('./middleware/errorhandler')
-const errorhandler = require('./middleware/errorhandler')
-
+// const errorHandler = require('./middleware/errorhandler')
 
 app.use(cors())
 
@@ -17,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use(router)
-app.use(errorhandler)
+// app.use(errorhandler)
 
 app.listen(PORT, () => {
     console.log(`this server listening on ${PORT}`)
