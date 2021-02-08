b2fc9f615df40e0c21a6d84cc6786e070bf1936c
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-02-08 17:39:40 +0700

    firebase deploy

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-const base_url = 'http://localhost:3000/';
+const base_url = 'https://todo-app-stanly.herokuapp.com/';
 
 function onSignIn(googleUser) {
   // var profile = googleUser.getBasicProfile();
