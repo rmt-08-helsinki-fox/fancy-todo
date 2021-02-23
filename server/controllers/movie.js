f6e932358bbfab5b55774ed6a3b5aff49df7c695
iqballbaihaqi <baihaqiiqbal323@gmail.com>
2021-02-23 12:58:27 +0700

    revisi

--- server/controllers/movie.js
+++ server/controllers/movie.js
@@ -7,7 +7,20 @@ class MovieController {
     axios.get(`${baseUrl}/discover/movie?api_key=0325b4988316cbf381d422df2916c1c2&sort_by=popularity.desc&page=${req.query.page || ''}`)
     .then( response => {
       response.data.results.forEach( mov => {
-        mov.poster_path = 'https://www.themoviedb.org/t/p/w220_and_h330_face' + mov.poster_path
+        mov.poster_path = 'https://image.tmdb.org/t/p/w500' + mov.poster_path
+      })
+      res.json(response.data)
+    })
+    .catch( err => {
+      res.send(err)
+    })
+  }
+
+  static getBySearch(req, res) {
+    axios.get(`${baseUrl}/search/movie?api_key=0325b4988316cbf381d422df2916c1c2&language=en-US&query=${req.params.search}&page=1&include_adult=false`)
+    .then( response => {
+      response.data.results.forEach( mov => {
+        mov.poster_path = 'https://image.tmdb.org/t/p/w500' + mov.poster_path
       })
       res.json(response.data)
     })
--- server/routes/movie.js
+++ server/routes/movie.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const { MovieController } = require("../controllers");
 
 router.get('/', MovieController.getAll);
+router.get('/search/:search', MovieController.getBySearch)
 
 module.exports = router;
