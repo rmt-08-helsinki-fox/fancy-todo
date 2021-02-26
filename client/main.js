e8e6fb2fe0a50eb60eb98e7594793ab03e915bb2
Zalfa <zalfa.muhammad@gmail.com>
2021-02-26 13:01:40 +0700

    add todo & list todo tp msh berantakan

--- client/main.js
+++ client/main.js
@@ -6,25 +6,19 @@ const formRegister = $("#register-form")
 const formLogin = $("#form-login")
 const home = $(".home")
 const homeAfter = $("#home-after-login")
-const addTodo = $("#add-todo")
 const submitRegister = $("#register-submit")
 const submitLogin = $("#login-submit")
-const btnAddTodo = $("#add-todo")
 
 $(document).ready(() => {
-    auth()
+  auth()
 })
 
 btnRegister.click(() => {
-    pageRegister()
+  pageRegister()
 })
 
 btnLogin.click(() => {
-    pageLogin()
-})
-
-btnAddTodo.click(() => {
-  getTodo()
+  pageLogin()
 })
 
 btnLogout.click(() => {
@@ -36,10 +30,10 @@ function hideFuture() {
     btnLogin.hide()
     btnLogout.hide()
     home.hide()
-    addTodo.hide()
     formLogin.hide()
     formRegister.hide()
     homeAfter.hide()
+
 }
 
 function pageRegister() {
@@ -69,8 +63,15 @@ function handleRegister(data) {
     formRegister.trigger('reset')
     pageLogin()
   })
-  .fail(err => {
-    console.log(err, `error register from ajax`);
+  .fail(xhr => {
+    $('#password-regis').val('')
+    Swal.fire({
+      title: 'Oops error',
+      text: xhr.responseJSON.message[0],
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    })
+    // console.log(err, `error register from ajax`);
   })
   .always(() => {
     $("#email").val('')
@@ -103,13 +104,20 @@ function handleLogin(data) {
     data
   })
   .done(res => {
-    console.log(res, '<<<<<');
+    // console.log(res, '<<<<<');
     localStorage.setItem('access_token', res.access_token)
     formLogin.trigger('reset')
     auth()
   })
-  .fail(err => {
-    console.log(err.responseText, `error login from ajax`);
+  .fail(xhr => {
+    $("#login-password").val('')
+    Swal.fire({
+      title: 'Oops error',
+      text: xhr.responseJSON.message,
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    })
+    // console.log(err.responseText, `error login from ajax`);
   })
   .always(() => {
     $("#login-email").val('')
@@ -117,31 +125,142 @@ function handleLogin(data) {
   })
 }
 
-// function onSignIn(googleUser) {
-//   const id_token = googleUser.getAuthResponse().id_token
-//   // console.log(id_token);
-//   $.ajax({
-//     method: 'POST',
-//     url: `${baseUrl}googlelogin`,
-//     data:{
-//       id_token
-//     }
-//   })
-//   .done(res => {
-//     localStorage.setItem('access_token', res.access_token)
-//     auth()
-//   })
-//   .fail(err =>{
-//     console.log(`${err},error google login from ajax`);
-//   })
-//   .always(() => {
-//     console.log(`di always`);
-//   })
-// }
+function onSignIn(googleUser) {
+  const id_token = googleUser.getAuthResponse().id_token
+  // console.log(id_token);
+  $.ajax({
+    method: 'POST',
+    url: `${baseUrl}googlelogin`,
+    data:{
+      id_token
+    }
+  })
+  .done(res => {
+    localStorage.setItem('access_token', res.access_token)
+    auth()
+  })
+  .fail(xhr =>{
+    console.log(`${xhr}, >>>>>>> error google login from ajax`);
+    Swal.fire({
+      title: 'Oops error',
+      text: xhr.responseJSON.message,
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    })
+  })
+  // .always(() => {
+  //   console.log(`di always`);
+  // })
+}
 
 function getTodo() {
-
+  $.ajax({
+    method: 'GET',
+    url: `${baseUrl}todos`,
+    headers: {
+      access_token: localStorage.access_token
+    }
+  })
+  .done(data => {
+    $("#todo-list").empty()
+    $.each(data, (index, data) => {
+      let status = `<input type="checkbox" class="form-check-input" id="status-${data.id}" onclick="editStatus(${data.id}, '${data.status}')" `
+            if (data.status === true) {
+                status += `checked>`
+            } else if(data.status === false){
+                status += `>`
+            }
+                // console.log(data)
+                $('#todo-list').append(`
+                <div class="card" id="list-task" style="width: 40rem";>
+                    <div class="card-body todo-list-card" id=todoCard${data.id}>
+                        <div id=todoCardBody${data.id}>
+                            ${status}
+                            <h5 class="card-title">${data.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${data.due_date.split('T')[0]}</h6>
+                            <p class="card-text">${data.description}</p>
+                            <a href="#" onclick='editForm(${data.id})' class="card-link btn btn-primary" id="editTodo">Edit</a>
+                            <a href="#" onclick='deleteTodo(event, ${data.id})' class="card-link btn btn-danger" id="deleteTodo">Hapus</a>
+                        </div>
+                    </div>
+                </div>
+            `)
+    })
+  })
+  .fail(xhr => {
+    Swal.fire({
+      title: 'Oops error',
+      text: xhr.responseJSON.message,
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    })
+  })
 }
+
+$('#btn-add-todo').click((event) => {
+  event.preventDefault()
+  $('#add-form').show()
+})
+
+$('#add-submit').click((event) => {
+  event.preventDefault()
+  addTodo()
+})
+
+function addTodo() {
+  let title = $('#todo-tile').val()
+  let description = $('#todo-description').val()
+  let due_date = $('#todo-date').val()
+  // ajax
+  $.ajax({
+      method: 'POST',
+      url: `${baseUrl}todos`,
+      headers: {
+          access_token: localStorage.access_token
+      },
+      data: {
+         title,
+         description,
+         due_date
+      }
+  })
+  .done(response => {
+      getTodo()
+      $('#add-todo-form').hide()
+  })
+  .fail(err => {
+      console.log(err, "ini error dari ajax");
+  })
+  .always(()=>{
+  })
+}
+
+$(`#add-cancel-btn`).click(function (event) {
+event.preventDefault()
+$('#add-form').hide()
+})
+
+function deleteTodo(event, id) {
+  event.preventDefault
+  $.ajax({
+      method: 'DELETE',
+      url: `${baseUrl}todos/${id}`,
+      headers: {
+          access_token: localStorage.access_token
+      }
+  })
+  .done(response => {
+    getTodo()
+  })
+  .fail(err => {
+    console.log(err, "ini error dari ajax");
+  })
+  .always(()=>{
+  })
+}
+
+
+
 
 function auth() {
   if(localStorage.getItem('access_token')) {
@@ -162,8 +281,9 @@ function logout() {
 
 function mainPage() {
   hideFuture()
+  getTodo()
+  // formLogin.hide()
   btnLogout.show()
-  addTodo.show()
   homeAfter.show()
 }
 
